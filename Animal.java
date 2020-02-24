e2de4b51bb9401d1181a08915eda2f66c0f16e6c
James Villemarette <jvill@udel.edu>
2020-02-24 18:29:11 -0500

    made MyCompare3 functional

---
+++ Animal.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Animal {
+	
+	String name;
+	int legs;
+
+	public Animal(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+	
+	public int getLegs() {
+		return this.legs;
+	}
+	
+	public String toString() {
+		return this.name + " " + this.legs;
+	}
+	
+}
---
+++ Dog.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Dog extends Animal {
+	
+	public Dog(String name, int legs) {
+		super(name, legs);
+	}
+
+}
